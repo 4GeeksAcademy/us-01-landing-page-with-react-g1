5afebb6f427b586be1ddbeccb963c635e08e8e52
Jose Alejandro Moreno <166447988+JochiKum@users.noreply.github.com>
2024-08-08 20:17:26 +0000

    feat:card

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -1,17 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
+import logoHCHT from '../../img/rigo-baby.jpg';
 
-export const Cards = () => {
+export const Cards = (props) => {
 
     return (
         <div className="container">
-            <div className="card" >
-                <img src="..." className="card-img-top" alt="..." />
+            <div className="card">
+                <img src={props.logo} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text">{props.description}</p>
+                    <a href="#" className="btn btn-primary">Find Out More!</a>
                 </div>
             </div>
         </div>
     )
 }
+
+Cards.propTypes = {
+    title: PropTypes.string,
+    description: PropTypes.string,
+    logo: PropTypes.string,
+  }
+  
+  Cards.defaultProps = {
+    title: 'Baby Rigo',
+    description: "Brief description of the image of baby rigo",
+    logo: logoHCHT,
+  }
