8eae8469360efff960c18088972986906fd6c5cd
mamel210 <mamel210@gmail.com>
2024-08-08 00:19:16 +0000

    navbar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Nabvar } from "./Nabvar";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
+			<Nabvar/>
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
