cb3db4737fe7f0200f9b62e64ec890b5ffdf7ddd
mamel210 <mamel210@gmail.com>
2024-08-09 20:23:47 +0000

    feat: home

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import { Jumbotron } from "./Jumbotron";
-import { Nabvar } from "./Nabvar";
-import { Cards } from "./Cards";
+import { Jumbotron } from "./Jumbotron.jsx";
+import { Nabvar } from "./Nabvar.jsx";
+import { Cards } from "./Cards.jsx";
+import { Footer } from "./Footer.jsx";
 
 //create your first component
 const Home = () => {
@@ -12,24 +13,13 @@ const Home = () => {
 		<div className="text-center p-5">
 			<Nabvar />
 			<Jumbotron />
-				<div className="row row-cols-4 mt-4 display: blockquote">
-					<Cards/>
-					<Cards title='Baby Rigo' description='Brief description about the image of baby rigo'/>
-					<Cards title='Hello World' description='Brief description about the image of baby rigo'/>
-					<Cards title='ready' description='Brief description about the image of baby rigo'/>
+			<div className="row row-cols-4 mt-4 display: blockquote">
+				<Cards />
+				<Cards title='Baby Rigo' description='Brief description about the image of baby rigo' />
+				<Cards title='Hello World' description='Brief description about the image of baby rigo' />
+				<Cards title='ready' description='Brief description about the image of baby rigo' />
 			</div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Footer/>			
 		</div>
 	);
 };
